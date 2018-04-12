5ac2ef8d8d428532914d0eb7576397917b98c7d4
Joseph Nehl <jo3n3hl@gmail.com>
2018-04-11 18:08:12 -0700

    started password recovery by email

--- TM/src/tm/Panels/pnlAccountRecovery.java
+++ TM/src/tm/Panels/pnlAccountRecovery.java
@@ -10,14 +10,18 @@ import javax.mail.internet.*;
  */
 public class pnlAccountRecovery extends javax.swing.JFrame {
 
-    /**
-     * Creates new form pnlAccountRecovery
-     */
+    private static String USER_NAME = "db.auto.messenger";
+    private static String PASSWORD = "qwerty123!!";
+    
     public pnlAccountRecovery() {
+        initComponents();
+    }
+    
+    public pnlAccountRecovery(String email) {
         String rand="";
         setResizable(false);
         rand=createRandom();
-        sendEmail(rand);
+        sendEmail(rand, email);
         initComponents();
     }
 
@@ -140,9 +144,57 @@ public class pnlAccountRecovery extends javax.swing.JFrame {
         return uuid;
     }
     
-    private void sendEmail(String id){
-        
+    private void sendEmail(String id, String email){
+        String from = USER_NAME;
+        String pass = PASSWORD;
+        String[] to = { email };
+        String subject = "Java send mail example";
+        String body = "Welcome to JavaMail!";
+
+        sendFromGMail(from, pass, to, subject, body);
     }
+    
+    private void sendFromGMail(String from, String pass, String[] to, String subject, String body){
+        Properties props = System.getProperties();
+        String host = "smtp.gmail.com";
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.user", from);
+        props.put("mail.smtp.password", pass);
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+
+        Session session = Session.getDefaultInstance(props);
+        MimeMessage message = new MimeMessage(session);
+
+        try {
+            message.setFrom(new InternetAddress(from));
+            InternetAddress[] toAddress = new InternetAddress[to.length];
+
+            // To get the array of addresses
+            for( int i = 0; i < to.length; i++ ) {
+                toAddress[i] = new InternetAddress(to[i]);
+            }
+
+            for( int i = 0; i < toAddress.length; i++) {
+                message.addRecipient(Message.RecipientType.TO, toAddress[i]);
+            }
+
+            message.setSubject(subject);
+            message.setText(body);
+            Transport transport = session.getTransport("smtp");
+            transport.connect(host, from, pass);
+            transport.sendMessage(message, message.getAllRecipients());
+            transport.close();
+        }
+        catch (AddressException ae) {
+            ae.printStackTrace();
+        }
+        catch (MessagingException me) {
+            me.printStackTrace();
+        }
+    }
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
